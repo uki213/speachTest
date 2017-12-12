ee747cae46193b87dba15d028a35e5942da019d8
Niimi Youki <niimiyouki@gmail.com>
2017-12-12 14:59:42 +0900

    音声受付部分仮組み

---
+++ index.js
@@ -0,0 +1,14 @@
+// 言語設定
+var speech = new webkitSpeechRecognition();
+speech.lang = "ja";
+
+// エレメント設定
+var btn = document.getElementById('btn');
+
+btn.addEventListener('click', function () {
+    speech.start();
+})
+
+speech.addEventListener('result', function (e) {
+    alert(e);
+});
