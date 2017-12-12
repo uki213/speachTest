4c3580a0cdb22d45ac78894d7ba95febfd993f9f
Niimi Youki <niimiyouki@gmail.com>
2017-12-12 17:40:25 +0900

    受けた文字を音声で返す

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 // 言語設定
-var speech = new webkitSpeechRecognition();
+const speech = new webkitSpeechRecognition();
 speech.lang = 'ja';
 
 obj = {test:'aaa'}
 
 // エレメント設定
-var btn = document.getElementById('btn');
+const btn = document.getElementById('btn');
 
 btn.addEventListener('click', function () {
     speech.start();
 })
 
 speech.addEventListener('result', function (e) {
-    document.querySelector('.content').innerHTML = e.results.item(0).item(0).transcript;
+    const retrunText = e.results.item(0).item(0).transcript;
+    document.querySelector('.content').innerHTML = retrunText
+
+    var synthes = new SpeechSynthesisUtterance(
+        retrunText
+    );
+    synthes.lang = "ja-JP"
+    speechSynthesis.speak( synthes );
+    
 });
